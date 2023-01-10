54d63fe5b2486c4411169b8bc925e151a036527a
Mahmoud <mahmoud.talass@gmail.com>
2023-01-10 15:31:04 -0500

    Remove values array and add num1 and num2 vars
    num1 and num2 will hold the first and second inputs that
    the operation will be done on.
    create a doOperation function that takes in two parameters.
    the operation symbol and the actual operation.
    The function first checks if num2 is undefined, and if so,
    it checks if num1 exists, if it does, display num1
    and the sign operation symbol in the fullOperationDis.
    if num1 does not exist, set num1 to the currentNumDisVal
    and then display num1 with the operation symbol after in the
    fullOperationDis.
    
    After this, set currentNumDis to nothing and assign the
    operation variable to the op parameter. this will depend on each
    operation.
    
    Craete an event listner that runs the doOperation function
    for each of the operations (plus,minus,divide, and times).
    
    Create an event listener to the equals buttons. When clicked,
    check if num1 exists, if it does, set num2 to currentNumDisVal
    display num2 in the fullOperationDis. Then check if num2 is equals
    to 0 and the operation is division, if so, give an error message.
    if not create a result variable that will run the operate
    function with the paremeters of the operation, num1 and num2.
    set currentNumDis to the display the result. Set num1 to the
    result, and num2 to undefined.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const fullOperationDis = document.querySelector("#full-operation");
 let fullOperationDisVal = fullOperationDis.textContent;
-const currentNumDis = document.querySelector("#current-num")
+const currentNumDis = document.querySelector("#current-num");
 let currentNumDisVal = currentNumDis.textContent;
 
 const numBtns = document.querySelectorAll(".num-btn");
@@ -10,20 +10,43 @@ const plus = document.querySelector("#plus");
 const minus = document.querySelector("#minus");
 const divide = document.querySelector("#divide");
 const times = document.querySelector("#times");
+const equals = document.querySelector("#equals");
 
-let values = []
+// let values = [];
+let num1;
+let num2;
 let operation;
 
-plus.addEventListener(("click"), () => {
-   values.push(currentNumDisVal);
-   currentNumDis.textContent = ''
-   if (fullOperationDis.textContent === "") {
-      fullOperationDis.textContent += `${values[values.length - 1]}`
-   } else {
-      fullOperationDis.textContent += ` + ${values[values.length - 1]}`
+equals.addEventListener("click", () => {
+   if (num1) {
+      num2 = parseInt(currentNumDisVal);
+      fullOperationDis.textContent += `${num2} =`;
+      if (operation === division && num2 === 0) {
+         currentNumDis.textContent = "error: you cannot divide by 0";
+      } else {
+         let result = operate(operation, num1, num2);
+         currentNumDis.textContent = result;
+         num1 = result;
+         num2 = undefined;
+      }
    }
-   operation = add();
-})
+});
+
+plus.addEventListener("click", () => {
+   doOperation("+", add);
+});
+
+minus.addEventListener("click", () => {
+   doOperation("-", subtract);
+});
+
+divide.addEventListener("click", () => {
+   doOperation("÷", division);
+});
+
+times.addEventListener("click", () => {
+   doOperation("•", multiply);
+});
 
 numBtnsArr.map((btn) => {
    btn.addEventListener("click", () => {
@@ -31,6 +54,19 @@ numBtnsArr.map((btn) => {
       currentNumDisVal = currentNumDis.textContent;
    });
 });
+
+function doOperation(sign, op) {
+   if (num2 === undefined) {
+      if (num1) {
+         fullOperationDis.textContent = `${num1} ${sign} `;
+      } else {
+         num1 = parseInt(currentNumDisVal);
+         fullOperationDis.textContent += `${num1} ${sign} `;
+      }
+      currentNumDis.textContent = "";
+      operation = op;
+   }
+}
 
 function add(...args) {
    return args.reduce((total, currentNum) => {
